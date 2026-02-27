40b9c20c4a5079319dcb89d63d9e723df7d5a63d
Stanislav Koshynskyi <stasony10@gmail.com>
2026-02-27 19:42:54 +0200

    feat: auth service

---
+++ backend/src/main/java/com/pupuputeam/backend/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.pupuputeam.backend.service;
+
+import com.pupuputeam.backend.entity.User;
+import com.pupuputeam.backend.exception.AuthException;
+import com.pupuputeam.backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    public String login(String userEmail, String password) {
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new AuthException("User not found"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new AuthException("Wrong password");
+        }
+
+        return jwtService.generateToken(userEmail);
+    }
+}
