04398f563ae2cb8364ebd3048d4e292f23ed7840
Stanislav Koshynskyi <stasony10@gmail.com>
2026-02-28 18:48:39 +0200

    feat: add validation

--- backend/src/main/java/com/pupuputeam/backend/dto/request/OrderFilterDto.java
+++ backend/src/main/java/com/pupuputeam/backend/dto/request/OrderFilterDto.java
@@ -1,16 +1,25 @@
 package com.pupuputeam.backend.dto.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
 public record OrderFilterDto(
         BigDecimal minAmount,
+        @PositiveOrZero(message = "max amount cannot be negative")
         BigDecimal maxAmount,
+        @PastOrPresent(message = "start time cannot be in future")
         Instant startTime,
         Instant endTime,
         String countyName,
         String muniName,
         BigDecimal minTaxRate,
+        @PositiveOrZero(message = "max tax rate cannot be negative")
+        @DecimalMax(value = "1.0",message = "tax rate cannot be above than 100%")
         BigDecimal maxTaxRate
 ){
 }
