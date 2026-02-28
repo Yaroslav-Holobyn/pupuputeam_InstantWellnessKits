cfde8b5095188ee599e2b7556a5bd1cf9e022722
Stanislav Koshynskyi <stasony10@gmail.com>
2026-02-28 20:51:47 +0200

    feat: add logging

--- backend/src/main/java/com/pupuputeam/backend/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/pupuputeam/backend/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.pupuputeam.backend.controller;
 import com.pupuputeam.backend.dto.response.ErrorResponse;
 import com.pupuputeam.backend.exception.AuthException;
 import com.pupuputeam.backend.exception.InvalidLocationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -13,13 +14,14 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        log.warn("Parameter mismatch: {}", ex.getName());
         ErrorResponse response = new ErrorResponse("Invalid Parameter Type",
                 "Incorrect format to '" + ex.getName() + "'. Expected type: " + ex.getRequiredType().getSimpleName());
 
@@ -28,22 +30,30 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ErrorResponse> handleBindException(BindException ex) {
-        ErrorResponse response = new ErrorResponse("Data binding error","Data binding error. Check the parameters you sent." )
+        log.error("Binding error: {}", ex.getMessage());
+        ErrorResponse response = new ErrorResponse("Data binding error",
+                ex.getBindingResult().getFieldErrors().stream()
+                        .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                        .findFirst()
+                        .orElse("Data binding error"));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<ErrorResponse> handleAuthErrors(AuthException ex) {
+        log.warn("Auth attempt failed: {}", ex.getMessage());
         ErrorResponse response = new ErrorResponse("Unauthorized", "Wrong email or password");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
     @ExceptionHandler(InvalidLocationException.class)
     public ResponseEntity<ErrorResponse> handleInvalidLocationException(InvalidLocationException ex) {
+        log.warn("Invalid location: {}", ex.getMessage());
         ErrorResponse response = new ErrorResponse("Invalid location", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+        log.error("General error: {}", ex);
         ErrorResponse response = new ErrorResponse("Internal Server Error", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
